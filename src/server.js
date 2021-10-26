485410052d43b5a7b45e82b93ee748ec375fd4c6
chanhong-ji <jichanhong24@gmail.com>
2021-10-26 17:54:14 +0900

    Setup Express

--- src/server.js
+++ src/server.js
@@ -2,4 +2,14 @@ import express from "express";
 
 const app = express();
 
-app.listen(4000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
+const onListening = () => console.log(`Server has openen port ${PORT}🌍`);
+
+const PORT = 4000;
+
+app.listen(PORT, onListening);
